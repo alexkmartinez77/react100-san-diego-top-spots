bb6ad32d60d7928a0771990efce7e969d30530c5
Alex Martinez <alexkmartinez77@gmail.com>
2021-10-14 16:45:40 -0700

    Adds Header (title/subtitle).

--- src/app.jsx
+++ src/app.jsx
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
+import Header from './header';
 
 class App extends Component {
   render() {
     return (
       <div className='App'>
+        <div className="container">
+          <Header />
+        </div>
       </div>
     );
   }
