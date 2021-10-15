2fac78e471477e35da6563e4c5a32ccc17001fd5
Alex Martinez <alexkmartinez77@gmail.com>
2021-10-15 14:20:58 -0700

    Adds google fonts and icons, a top spots button that acts as a link to open a new google map window focused on the respective top spot.

--- src/header.jsx
+++ src/header.jsx
@@ -3,9 +3,13 @@ import React from 'react';
 export class Header extends React.Component{
  render(){
    return (
-     <div className="card rounded bg-light mb-2 mx-2 mt-4 p-2">
-          <h1>San Diego Top Spots</h1>
-          <p>A list of the top 30 places to see in San Diego, California.</p>
+     <div>
+      <div className="card rounded bg-light mb-2 mx-2 mt-4 p-2 " style={{backgroundImage: `url("https://i.stack.imgur.com/B6fEt.png")`}}>
+            <h1 className="promptFont display-1">San Diego Top Spots</h1>
+      </div>
+      <div className="m-2 p-2">
+        <h5 className="promptFont whiteFont">A list of the top 30 places to see in San Diego, California.</h5>
+      </div>
      </div>
    )
  }
--- src/topspot.jsx
+++ src/topspot.jsx
@@ -4,10 +4,27 @@ export class TopSpot extends React.Component{
   render(){
     let {name, description, location: [long, lat],} = this.props.obj;
     return (
-      <div className="card rounded bg-light m-2 p-2">
-        <h4>{name}</h4>
-        <p>{description}</p>
-        <a href={`https://www.google.com/maps?q=${long},${lat}`}>Google Map It</a>
+      <div className="row well rounded bg-light m-2 p-2">
+        <div className="row">
+          <h4>{name}</h4>
+          <p>{description}</p>
+        </div>
+        <div className="row">
+          <div className="col text-center">
+            <a className="googleLink" href={`https://www.google.com/maps?q=${long},${lat}`} target="_blank">
+              <button type="button" className="btn btn-info">
+                  <span className="material-icons redFont">location_on</span> 
+                  <span className="blueFont prompt">G</span>
+                  <span className="orangeFont prompt">o</span>
+                  <span className="orangeFont prompt">o</span>
+                  <span className="blueFont prompt">g</span>
+                  <span className="greenFont prompt">l</span>
+                  <span className="redFont prompt">e</span>
+                  <span className="blackFont prompt"> Map It</span>
+                </button>
+              </a>
+            </div>
+        </div>
       </div>
   
     )
