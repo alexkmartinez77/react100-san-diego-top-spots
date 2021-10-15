023addeb89ad9d4293d250e4dfb9818c3d0f0bbd
Alex Martinez <alexkmartinez77@gmail.com>
2021-10-14 18:17:19 -0700

    Adds function componentwillmount() which  accesses json data using a get request via axios and then populates our state array with this data so that it lives locally.

--- src/app.jsx
+++ src/app.jsx
@@ -1,12 +1,34 @@
 import React, { Component } from 'react';
 import Header from './header';
+import axios from 'axios';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      topspots: [],
+    }
+  }
+
+  componentWillMount() {
+    axios
+    .get('https://origin-top-spots-api.herokuapp.com/api/topspots')
+    .then(response => response.data)
+    .then(topspots => this.setState({ topspots }));
+  }
+
   render() {
     return (
       <div className='App'>
         <div className="container">
           <Header />
+          <div className="row">
+            <div className="col">
+              <pre>
+                { JSON.stringify(this.state.topspots, null, 2) }
+              </pre>
+            </div>
+          </div>
         </div>
       </div>
     );
--- src/header.jsx
+++ src/header.jsx
@@ -4,10 +4,8 @@ export class Header extends React.Component{
  render(){
    return (
      <div className="row">
-       <div col>
           <h1 className="row display text-center">San Diego Top Spots</h1>
-          <p className="row">A list of the top 30 places to see in San Diego, California.</p>
-       </div>
+          <p>A list of the top 30 places to see in San Diego, California.</p>
      </div>
    )
  }
